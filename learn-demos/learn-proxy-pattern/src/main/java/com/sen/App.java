cfa9df5525739b8ef47aad6ae0be46054d68509d
huangjs <jiesen1.27@live.cn>
2020-01-02 09:42:08 +0800

    proxy

---
+++ learn-demos/learn-proxy-pattern/src/main/java/com/sen/App.java
@@ -0,0 +1,11 @@
+package com.sen;
+
+/**
+ * @author HuangJS
+ * @date 2019-12-31 5:10 下午
+ */
+public class App {
+
+
+
+}
---
+++ learn-demos/learn-proxy-pattern/src/main/java/com/sen/staticproxy/jdkproxy/ProxyUtil.java
@@ -0,0 +1,13 @@
+package com.sen.staticproxy.jdkproxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @author HuangJS
+ * @date 2020-01-02 9:39 上午
+ */
+public class ProxyUtil {
+    public Object newProxyInstance() {
+        Proxy.newProxyInstance()
+    }
+}
