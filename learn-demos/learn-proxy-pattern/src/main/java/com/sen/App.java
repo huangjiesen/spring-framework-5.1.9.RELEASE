2e241ec53986a0bc9b5401dc5859270447a993ee
huangjs <jiesen1.27@live.cn>
2020-01-02 18:44:06 +0800

    实现接口及类的动态代理

--- learn-demos/learn-proxy-pattern/src/main/java/com/sen/App.java
+++ learn-demos/learn-proxy-pattern/src/main/java/com/sen/App.java
@@ -1,11 +1,99 @@
 package com.sen;
 
+import java.lang.reflect.Method;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * @author HuangJS
  * @date 2019-12-31 5:10 下午
  */
 public class App {
 
+    public static void main(String[] args)  {
+
+        Method sss3 = null;
+        try {
+            Class.forName("com.sss");
+            sss3 = Test3.class.getDeclaredMethod("sss5",new Class<?>[]{});
+        } catch (NoSuchMethodException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(sss3.getName());
+
+        for (Method method : Test4.class.getMethods()) {
+
+            System.out.println(method.getName()+"  ->   "+method.getDeclaringClass());
+        }
+
+
+        System.out.println("--------------");
+        for (Method method : Test4.class.getDeclaredMethods()) {
+            System.out.println(method.getName()+"  ->   "+method.getDeclaringClass());
+        }
+    }
+
+    public void sss() {
+
+    }
+    public void sss1() {
+
+    }
+
+    public  interface Test1 {
+        void sss1();
+        void sss2();
+        void sss3();
+    }
+
+    public interface Test2 {
+        LocalDateTime sss4(LocalDate now, Test3 test);
+        void sss5();
+        void sss6();
+    }
+
+    public static class Test4 extends Test3 implements Test1 {
+
+        @Override
+        public void sss1() {
+
+        }
+
+        @Override
+        public void sss2() {
+
+        }
+
+        protected void seeee() {
+
+        }
+        @Override
+        public void sss3() {
+
+        }
+    }
+    public static class Test3  implements Test2 {
+        @Override
+        public LocalDateTime sss4(LocalDate now,Test3 test) {
+            System.out.println("now is " + now);
+            return now.atTime(22,34);
+        }
+
+        @Override
+        public void sss5() {
+
+        }
+
+        @Override
+        public void sss6() {
+
+        }
+
+        public Runtime getRuntime(LocalDate now,Test3 test) {
+            System.out.println();
+            return null;
+        }
+    }
 
 
 }
--- learn-demos/learn-proxy-pattern/src/main/java/com/sen/staticproxy/jdkproxy/ProxyUtil.java
+++ learn-demos/learn-proxy-pattern/src/main/java/com/sen/staticproxy/jdkproxy/ProxyUtil.java
@@ -1,13 +1,125 @@
 package com.sen.staticproxy.jdkproxy;
 
-import java.lang.reflect.Proxy;
+import com.sen.App;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
 
 /**
  * @author HuangJS
  * @date 2020-01-02 9:39 上午
  */
-public class ProxyUtil {
-    public Object newProxyInstance() {
-        Proxy.newProxyInstance()
+public class ProxyUtil  {
+    public static Object newProxyInstance(ClassLoader loader, Class<?> target, InvocationHandler handler) throws Exception{
+        String distDir = ProxyUtil.class.getClassLoader().getResource("").getPath()+"/";
+        File packageDir = new File(distDir + target.getPackage().getName().replaceAll("\\.", "/"));
+        if (!packageDir.exists()) {
+            packageDir.mkdirs();
+        }
+        Path path = Paths.get(packageDir.getPath() + "/$" + target.getSimpleName() + "Proxy.java");
+        Files.write(path, javaImplementsStr(target).getBytes(), StandardOpenOption.CREATE,StandardOpenOption.TRUNCATE_EXISTING);
+
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        int flag = compiler.run(null, null, null, path.toString());
+        if(flag!=0){
+            throw new RuntimeException("编译失败");
+        }
+
+
+        // 如果代理类生成在外部，则需要URLClassLoader进行加载，如D:/com/sen/$TestProxy.java
+        // loader = new URLClassLoader(new URL[]{new URL("file://D://")});
+
+        Class<?> aClass = loader.loadClass(target.getPackage().getName() + ".$" + target.getSimpleName() + "Proxy");
+        Constructor<?> declaredConstructor = aClass.getDeclaredConstructor(InvocationHandler.class);
+        return declaredConstructor.newInstance(handler);
+    }
+
+    private static String javaImplementsStr(Class<?> target) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("package " + target.getPackage().getName() + ";\n");
+        sb.append("import com.sen.staticproxy.jdkproxy.ProxyUtil.InvocationHandler;\n");
+        sb.append("import java.lang.reflect.Method;\n");
+
+        sb.append("public class $" + target.getSimpleName() + "Proxy " + (target.isInterface() ? "implements " : "extends ") + target.getCanonicalName() + " {\n");
+        sb.append("    private InvocationHandler handler;\n");
+        sb.append("    public $" + target.getSimpleName() + "Proxy(InvocationHandler handler){\n");
+        sb.append("        this.handler=handler;\n");
+        sb.append("    }\n");
+
+        for (Method method : target.getDeclaredMethods()) {
+            Class<?>[] types = method.getParameterTypes();
+            String args = "";
+            String params = "";
+            String argsType = "";
+            for (int i = 0; i < types.length; i++) {
+                argsType += "," + types[i].getCanonicalName() + ".class";
+                args += "," + types[i].getCanonicalName() + " p" + i;
+                params += ",p" + i;
+            }
+            if (args.length() > 0) {
+                args = args.substring(1);
+                params = params.substring(1);
+                argsType = argsType.substring(1);
+            }
+            String returnType = "void".equals(method.getReturnType().getName()) ? "" : "return (" + method.getReturnType().getName() + ")";
+
+            sb.append("    @Override\n");
+            sb.append("    public " + method.getReturnType().getName() + " " + method.getName() + "(" + args + ") {\n");
+            sb.append("        Method method = null;\n");
+            sb.append("        try {\n");
+            sb.append("            method = Class.forName(\"" + target.getTypeName() + "\").getDeclaredMethod(\"" + method.getName() + "\",new Class<?>[]{" + argsType + "});\n");
+            sb.append("        } catch (Exception e) {\n");
+            sb.append("             throw new RuntimeException(e);\n");
+            sb.append("        }\n");
+            sb.append("        " + returnType + "handler.invoke(this,method,new Object[]{" + params + "});\n");
+            sb.append("    }\n");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+
+    public interface InvocationHandler {
+        Object invoke(Object proxy, Method method, Object[] args);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        InvocationHandler handler = new InvocationHandler() {
+            App.Test4 test3 = new App.Test4();
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) {
+                try {
+                    Class<?> declaringClass = method.getDeclaringClass();
+                    if (Object.class.equals(declaringClass)) {
+                        System.out.println("method.invoke(proxy, args)");
+                        return method.invoke(proxy, args);
+                    }
+
+                    System.out.println("proxy logs --------------------");
+                    return method.invoke(test3, args);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+
+        // 类的代理，问题在于继承的方法是否要代理
+        App.Test4 test2 = (App.Test4)ProxyUtil.newProxyInstance(ProxyUtil.class.getClassLoader(), App.Test4.class, handler);
+
+
+        test2.sss4(LocalDate.now(), null);
+        System.out.println(test2.toString());
     }
 }
